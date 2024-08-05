5dbd8506ffc6f28ba778bd3b1545af007ce5c886
Katherine <43319584+kperryr@users.noreply.github.com>
2024-08-05 18:13:43 -0400

    ch 23 exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,31 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    button.addEventListener('click',event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.background = 'red';
+
+    });
+
+    missionAbort.addEventListener('mouseout', event => {
+        missionAbort.style.background = '';
+
+    });
+
+    missionAbort.addEventListener('click', event => {
+
+        const qa = window.confirm('Are you sure you want to abort the mission?');
+        if ( qa === true){
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+
+    });
+    
+
     
 }
 
